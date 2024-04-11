9ac5b58c1922837c1b39f09bac6e9f4e605f92cb
Vinicius_pc <afredovag@yahoo.com.br>
2024-04-11 12:22:44 -0300

    .

--- disparador de e-mails.py
+++ disparador de e-mails.py
@@ -13,13 +13,13 @@ def selecionar_anexo():
 
 
 # Criando integração do Python com o Outlook
-outlook = win32.Dispatch('outlook.application')
+outlook = win32.Dispatch("Outlook.Application")
 
 # Criando um e-mail
 email = outlook.CreateItem(0)
-emails = {} #não sei ainda como vou fazer a lista
-periodo = {} #ainda vou ver como criar a variável, deixei como lista só pra deixa o objeto periodo já criado
-medico = {} #Lista de médicos para iterar no código e enviar o e-mail para todos
+emails = "kelonios2000@yahoo.com.br" #não sei ainda como vou fazer a lista
+periodo = "10 de outubro de 2023 a 10 de abril de 2024" #ainda vou ver como criar a variável, deixei como lista só pra deixa o objeto periodo já criado
+medico = "Alfredo Vinícius" #Lista de médicos para iterar no código e enviar o e-mail para todos
 
 
 # Configurando as informações do e-mail
@@ -31,17 +31,11 @@ email.HTMLBody = f"""
 
 <p> Segue em anexo o relatório dos honorários médicos a respeito dos procedimentos pagos, faturados e a faturar referentes ao período de {periodo}</p>
 
-<p>Atenciosamente,</p>
-<p> Alfredo Vinícius Andrade Guimarães</p>
-<p> Setor de Faturamento</p>
-<p> Hospital de Nossa Senhora das Mercês</p>
-<p> Fone: (32)98447-5784 (WhatsApp e Telegram)</p>
-<img src="Logo hospital.png" />
 """
 
 
-anexo = selecionar_anexo()
-email.Attachments.Add(anexo)
+# anexo = selecionar_anexo()
+# email.Attachments.Add(anexo)
 
 
 email.Send()

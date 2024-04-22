e464fbd1cb9967961aa9dbaca7f16621858bcd44
Vinicius_pc <afredovag@yahoo.com.br>
2024-04-22 10:53:13 -0300

    .

--- menagens WhatsApp.py
+++ menagens WhatsApp.py
@@ -5,9 +5,17 @@ import time
 import csv
 from tkinter import Tk, filedialog
 from openpyxl import load_workbook
+import selenium as sl
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
 
 def selecionar_arquivo():
-    Tk().withdraw()  # Não mostrar a janela completa do Tk
+    root = Tk()
+    root.withdraw()  # Não mostrar a janela completa do Tk
+    root.attributes('-topmost', True)
     path_to_file = filedialog.askopenfilename(title="Selecione o arquivo Excel")
     return path_to_file
 
@@ -16,21 +24,19 @@ caminho_do_arquivo = selecionar_arquivo()
 
 
 # Carregar a planilha
-planilha = pd.read_excel(caminho_do_arquivo)
-wb = load_workbook(filename=caminho_do_arquivo)
-sheet = wb.active  # Assumindo que os dados estão na primeira aba
-
+planilha = pd.read_excel(caminho_do_arquivo, sheet_name='Plan1')
+contatos_df = pd.DataFrame(planilha)
+contatos_df = contatos_df[['Nome', 'Telefone']]
+contatos_df.dropna(subset=['Telefone'], inplace=True)
+contatos_df['Telefone'] = contatos_df['Telefone'].astype(str).str.replace('.0', '')
+contatos_df.head(20)
+contatos_df = contatos_df.drop(labels=[0,1,2,3,4,5,6,7], axis=0)
+contatos_df['Telefone'] = '55' + contatos_df['Telefone']
+contatos_df.head(20)
 # Inicializar um dicionário vazio
-dicionario = {}
+dicionario = contatos_df.set_index('Nome')['Telefone'].to_dict()
 
-# Supondo que a primeira linha tenha os cabeçalhos
-# e 'Name' e 'Home Phone' sejam os títulos das colunas desejadas
-for row in sheet.iter_rows(min_row=2, values_only=True):
-    nome = row[0]  # Assumindo que 'Name' esteja na primeira coluna
-    home_phone = row[1]  # Assumindo que 'Home Phone' esteja na segunda coluna
-    
-    dicionario[nome] = home_phone
 
-# Exibir o dicionário
+
 
 print(dicionario)

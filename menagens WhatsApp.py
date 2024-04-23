42670a1f0cf18f80b38de824edcda49fc68796bf
Vinicius_pc <afredovag@yahoo.com.br>
2024-04-23 09:26:52 -0300

    Alteração do código para envio de mensagens WP

--- menagens WhatsApp.py
+++ menagens WhatsApp.py
@@ -1,20 +1,8 @@
 import pandas as pd
-from selenium.webdriver.common.keys import Keys
-import pyperclip
-import time
-import csv
 from tkinter import Tk, filedialog
-from openpyxl import load_workbook
-import selenium as sl
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-import os
+import pyautogui as pag
 
+pag.PAUSE = 1
 
 def selecionar_arquivo():
     root = Tk()
@@ -23,14 +11,27 @@ def selecionar_arquivo():
     path_to_file = filedialog.askopenfilename(title="Selecione o arquivo Excel")
     return path_to_file
 
-def enviar_mensagem(nome, telefone):
-    # Criar a URL
-    url = f"https://wa.me/{telefone}"
-    driver.get(url)
-    time.sleep(10)  # Aguardar o carregamento da página
+def enviar_mensagem(nome, arquivo, mensagem):
+    try: 
+        
+        pag.click(x=188, y=106)
+        pag.write(f"{nome}")
+        pag.press('enter')
+        pag.click(x=542, y=696)
+        pag.click(x=597, y=306)
+        pag.write(arquivo)
+        pag.sleep(2)
+        pag.press('enter')
+        pag.sleep(2)
+        pag.write(mensagem.format(nome=nome, periodo=periodo_referencia))
+        pag.sleep(2)
+        pag.press('enter')
+        pag.sleep(4)
+        return f"Arquivo de {nome} enviado com sucesso!"
+    except Exception as nome:
+        return f"Não foi possível enviar o arquivo de {nome}."
 
-
-# Caminho do seu arquivo CSV
+# Selecionar o arquivo
 caminho_do_arquivo = selecionar_arquivo()
 
 
@@ -48,23 +49,19 @@ contatos_df.head(20)
 dicionario = contatos_df.set_index('Nome')['Telefone'].to_dict()
 print(dicionario)
 
-dicionario_teste = {'Vinicius': '5532988083456', 'Faturamento': '5532984475784'}
-
-# Configuração do driver do navegador (ajuste o caminho para o seu chromedriver)
-driver_path = '/path/to/your/chromedriver'
-driver = webdriver.Chrome(driver_path)
+nome_formatado = []
+nomes = contatos_df['Nome'].tolist()
 
 periodo_referencia = "10/Out/2023 a 10/Abr/2024"
-mensagem = "Prezado Dr(a). {nome}, segue em anexo o relatório de Honorários Médicos do período de referência compreendido entre {periodo}."
-
-# Função para enviar mensagens
 
 
-# Iterar sobre o dicionário de médicos
-for nome, telefone in dicionario.items():
-    enviar_mensagem(nome, telefone)
-
-driver.quit()  # Fechar o navegador
+# Iterar sobre a lista de médicos
+pag.click(x=227, y=752)
+for nome in nomes,:
+    nome_formatado = nome.upper()
+    arquivo = f"{nome_formatado}_relatorio.pdf"
+    mensagem = "Prezado Dr(a). {nome}, segue em anexo o relatório de Honorários Médicos do período de referência compreendido entre {periodo}."
+    enviar_mensagem(nome, arquivo, mensagem)
 
 
 

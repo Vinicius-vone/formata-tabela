7bfbb6c36a1e376b5620b1763898d36e187628f9
Vinicius_pc <afredovag@yahoo.com.br>
2024-04-23 10:27:04 -0300

    Ajuste do código

--- menagens WhatsApp.py
+++ menagens WhatsApp.py
@@ -13,57 +13,57 @@ def selecionar_arquivo():
 
 def enviar_mensagem(nome, arquivo, mensagem):
     try: 
-        
+        pag.sleep(2)
         pag.click(x=188, y=106)
+        pag.sleep(2)
         pag.write(f"{nome}")
+        pag.sleep(1)
         pag.press('enter')
+        pag.sleep(2)
         pag.click(x=542, y=696)
+        pag.sleep(2)
         pag.click(x=597, y=306)
+        pag.sleep(2)
         pag.write(arquivo)
         pag.sleep(2)
         pag.press('enter')
         pag.sleep(2)
-        pag.write(mensagem.format(nome=nome, periodo=periodo_referencia))
-        pag.sleep(2)
+        pag.write(mensagem)
+        pag.sleep(5)
         pag.press('enter')
-        pag.sleep(4)
+        pag.sleep(6)
         return f"Arquivo de {nome} enviado com sucesso!"
     except Exception as nome:
         return f"Não foi possível enviar o arquivo de {nome}."
 
 # Selecionar o arquivo
-caminho_do_arquivo = selecionar_arquivo()
+# caminho_do_arquivo = selecionar_arquivo()
 
 
-# Carregar a planilha
-planilha = pd.read_excel(caminho_do_arquivo, sheet_name='Plan1')
-contatos_df = pd.DataFrame(planilha)
-contatos_df = contatos_df[['Nome', 'Telefone']]
-contatos_df.dropna(subset=['Telefone'], inplace=True)
-contatos_df['Telefone'] = contatos_df['Telefone'].astype(str).str.replace('.0', '')
-contatos_df.head(20)
-contatos_df = contatos_df.drop(labels=[0,1,2,3,4,5,6,7], axis=0)
-contatos_df['Telefone'] = '55' + contatos_df['Telefone']
-contatos_df.head(20)
-# Inicializar um dicionário vazio
-dicionario = contatos_df.set_index('Nome')['Telefone'].to_dict()
-print(dicionario)
+# # Carregar a planilha
+# planilha = pd.read_excel(caminho_do_arquivo, sheet_name='Plan1')
+# contatos_df = pd.DataFrame(planilha)
+# contatos_df = contatos_df[['Nome', 'Telefone']]
+# contatos_df.dropna(subset=['Telefone'], inplace=True)
+# contatos_df['Telefone'] = contatos_df['Telefone'].astype(str).str.replace('.0', '')
+# contatos_df.head(20)
+# contatos_df = contatos_df.drop(labels=[0,1,2,3,4,5,6,7], axis=0)
+# contatos_df['Telefone'] = '55' + contatos_df['Telefone']
+# contatos_df.head(20)
+# # Inicializar um dicionário vazio
+# dicionario = contatos_df.set_index('Nome')['Telefone'].to_dict()
+# print(dicionario)
 
-nome_formatado = []
-nomes = contatos_df['Nome'].tolist()
+# nomes = contatos_df['Nome'].tolist()
 
-periodo_referencia = "10/Out/2023 a 10/Abr/2024"
+periodo_referencia = "10 de outubro de 2023 a 10 de abril de 2024"
 
+nomes = ['Celular Hospital', 'Vini']
 
 # Iterar sobre a lista de médicos
 pag.click(x=227, y=752)
-for nome in nomes,:
+for nome in nomes:
     nome_formatado = nome.upper()
     arquivo = f"{nome_formatado}_relatorio.pdf"
-    mensagem = "Prezado Dr(a). {nome}, segue em anexo o relatório de Honorários Médicos do período de referência compreendido entre {periodo}."
+    mensagem = f"Prezado Dr(a). {nome}, segue em anexo o relatório de Honorários Médicos do período de referência compreendido entre {periodo_referencia}."
     enviar_mensagem(nome, arquivo, mensagem)
-
-
-
-
-

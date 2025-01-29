1b3d15b0d5316a1043c2d14c7c26c567bb4478dd
Vinicius-vone <afredovag@yahoo.com.br>
2025-01-29 07:03:22 -0300

    ,

--- Formatação de tabelas para PDF - Modelo IV por médico -Completa.py
+++ Formatação de tabelas para PDF - Modelo IV por médico -Completa.py
@@ -452,7 +452,7 @@ def dados_sus_ambulatorio(file_path):
 
 
 path_to_file_pagos, path_to_file_nao_pagos, path_to_file_a_faturar, subtitulo, path_to_file_endo_pago, path_to_file_endo_nao_pago, path_to_file_sus_aih, path_to_file_sus_ambulatorio = selecionar_arquivo_e_diretorio()
-output_directory = "C:/Users/Pichau/Códigos Python/Relatórios Médicos/19-11-2024_17-12-2024"
+output_directory = "C:/Users/Pichau/Códigos Python/Relatórios Médicos/17-12-2024_21-01-2025"
 #FORMATAÇÃO DO DATAFRAME A PARTIR DO ARQUIVO TXT RETIRADO DIRETAMENTE DO SPDATA
 # Ler arquivo e criar lista
 lines_list_pagos = read_file_to_list(path_to_file_pagos)
